e92bee94268ef019d45d0927df333fc9642fe321
nicholas preis <nicholas_preis@pop-os.localdomain>
2020-10-12 16:35:39 -0400

    Program exits if there's no input.

--- color.sh
+++ color.sh
@@ -3,10 +3,13 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 blue='\033[0;34m'
-purple='\33[0;35m'
+magenta='\33[0;35m'
 default='\33[0m'
 
 read -p "Select a color (yellow or purple): " color
 color=${color^^}
 
-echo "${color}"
+if [ -z ${color} ]; then
+  echo "There is no color."
+  exit 1
+fi
