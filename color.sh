4e956435eefd2217e393a4b1ec966cfc6ae297f1
nicholas preis <nicholas_preis@pop-os.localdomain>
2020-10-12 14:05:09 -0400

    You can now input a color.

--- color.sh
+++ color.sh
@@ -3,5 +3,8 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 blue='\033[0;34m'
-magenta='\33[0;35m'
+purple='\33[0;35m'
 default='\33[0m'
+
+read -p "Select a color (yellow or purple): "
+color=${color^^}
