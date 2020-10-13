e776ae183eaec7f79255bf5475c10093990c8a3e
nicholas preis <nicholas_preis@pop-os.localdomain>
2020-10-13 12:03:57 -0400

    Made a statement to get a file.

--- hostlookup.sh
+++ hostlookup.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-for h in champlain.edu umv.edu umd.edu abcdefg.edu
+while getopts ":f:" options;
 do
-  out=$(host -W1 -t A $h)
-  if [ $? -eq 0 ]; then
-    ip=$(echo $out | cut -d " " -f 4)
-    echo $host - $ip
-  else
-    echo "{h} does not have a DNS record."
-  fi
+  case "${options}" in
+    f)
+      f=${OPTARG}
+      if [[ -f "$f" ]]; then
+        echo "Begin reading hosts:"
+      else
+        echo "Not a valid file."
+        exit 1
+      fi
+      ;;
+    *)
+  esac
 done
+
+while read line
+do
+  if [ ${f} == "hosts.txt" ]; then
+    for h in champlain.edu uvm.edu umd.edu abcdefg.hi
+    do
+      out=$(host -W1 -t A $h)
+      if [ $? -eq 0 ]; then
+        if [ out == "92.242.140.21" ] ; then
+          echo "{h} - not found"
+        else
+          ip=$(echo $out | cut -d " " -f 4)
+          echo ${h} - $ip
+        fi
+      fi
+    done
+  fi
+done < $f
+
+exit 0
