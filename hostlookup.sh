cd627327ee1915c17a7decdcf08494022c110a82
nicholas preis <nicholas_preis@pop-os.localdomain>
2020-10-12 22:40:14 -0400

    Added code to print out ip addresses.

--- hostlookup.sh
+++ hostlookup.sh
@@ -2,5 +2,11 @@
 
 for h in champlain.edu umv.edu umd.edu abcdefg.edu
 do
-  host -W1 -t A $h
+  out=$(host -W1 -t A $h)
+  if [ $? -eq 0 ]; then
+    ip=$(echo $out | cut -d " " -f 4)
+    echo $host - $ip
+  else
+    echo "{h} does not have a DNS record."
+  fi
 done
