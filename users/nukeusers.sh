80f2f3e822975097c27b254c85595b9df3f26e18
root <root@mymail.champlain.edu>
2020-10-22 23:47:33 -0400

    Fixed some things.

--- users/nukeusers.sh
+++ users/nukeusers.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-#Checks to see if the file is valid
-while getopts ":f:" options;
+for h in $(cat users.txt | cut -d "@" -f 1)
 do
-  case "${options}" in
-    f)
-      f=${OPTARG}
-      if [[ -f "$f" ]]; then
-        echo "Nuking Users"
-      else
-        echo "Not a valid file."
-        exit 1
-      fi
-      ;;
-    *)
-  esac
+  userdel -r $h
 done
 
-while read line
-do
-  for h in $line | cut -d "@" -f 1)
-  do
-    userdel -r $user
-  done
-done
