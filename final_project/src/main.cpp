b56002055540f2edee6839d7b36fd28a719b4fbb
root <root@mymail.champlain.edu>
2020-12-12 18:28:28 -0500

    Morked on main, and moved .h file to include folder.

--- final_project/src/main.cpp
+++ final_project/src/main.cpp
@@ -1,13 +1,46 @@
 #include<iostream>
+#include<ctime>
+#include<unistd.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     int opt{};
+    bool kmlFlag{false};
+    bool logFlag{false};
+    string logValue{};
+    string kmlValue;
+    bool optErr = true;
+    int currentLevel;
+
+    while((opt = getopt(argc, argv, "k:l:c:")) != EOF)
+    {
+        switch(opt)
+        {
+            case 'k':
+                kmlFlag = true;
+                kmlValue = optarg;
+                break;
+
+            case 'l':
+                logFlag = true;
+                logValue = optarg;
+                break;
+
+            case 'c':
+                currentLevel = (int)optarg;
+                break;
+
+            default:
+                optErr = true;
+                break;
+        }
+    }
     while(1)
     {
-
+        cout << "hello" << endl;
+        break;
     }
     return 0;
 }
